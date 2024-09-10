eb3be9d64cd855164e4a5141f99f61440eb1ea5a
Jarod <52184516+Razei@users.noreply.github.com>
2024-09-09 22:33:06 -0400

    install android and ios

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.example.app',
   appName: 'angular-capacitor-test',
-  webDir: 'dist'
+  webDir: 'dist/angular-capacitor-test/browser'
 };
 
 export default config;
